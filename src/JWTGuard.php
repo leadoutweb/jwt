85ab209e4dc6434fe0cbb981fc4090f7da89c272
Henrik Mikkelsen <henrik@leadout.dk>
2023-10-31 22:15:57 +0100

    Add attempt method

--- src/JWTGuard.php
+++ src/JWTGuard.php
@@ -2,11 +2,16 @@
 
 namespace Leadout\JWT;
 
+use Illuminate\Auth\Events\Attempting;
+use Illuminate\Auth\Events\Failed;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Auth\GuardHelpers;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Http\Request;
+use Illuminate\Support\Timebox;
 use Leadout\JWT\Entities\Token;
 use Leadout\JWT\Exceptions\JWTException;
 
@@ -15,43 +20,65 @@ class JWTGuard implements Guard
     use GuardHelpers;
 
     /**
-     * The HTTP request.
+     * The name of the guard.
      */
-    private Request $request;
+    protected string $name;
 
     /**
      * The token manager.
      */
-    private TokenManager $tokenManager;
+    protected TokenManager $tokenManager;
 
     /**
-     * The user provider.
+     * The event dispatcher.
      */
-    private UserProvider $userProvider;
+    protected Dispatcher $events;
+
+    /**
+     * The HTTP request.
+     */
+    protected ?Request $request;
+
+    /**
+     * The timebox.
+     */
+    protected Timebox $timebox;
 
     /**
      * Instantiate the class.
      */
-    public function __construct(Request $request, TokenManager $tokenManager, UserProvider $userProvider)
-    {
-        $this->request = $request;
+    public function __construct(
+        string $name,
+        UserProvider $provider,
+        TokenManager $tokenManager,
+        Dispatcher $events,
+        Request $request = null,
+        Timebox $timebox = null
+    ) {
+        $this->name = $name;
+
+        $this->provider = $provider;
 
         $this->tokenManager = $tokenManager;
 
-        $this->userProvider = $userProvider;
+        $this->events = $events;
+
+        $this->request = $request;
+
+        $this->timebox = $timebox ?: new Timebox;
     }
 
     /**
      * {@inheritDoc}
      */
-    public function user()
+    public function user(): ?Authenticatable
     {
         if ($this->user) {
             return $this->user;
         }
 
         try {
-            return $this->user = $this->userProvider->retrieveById(
+            return $this->user = $this->provider->retrieveById(
                 $this->tokenManager->decode($this->request)->claims()->sub()
             );
         } catch (JWTException) {
@@ -62,9 +89,9 @@ class JWTGuard implements Guard
     /**
      * {@inheritDoc}
      */
-    public function validate(array $credentials = [])
+    public function validate(array $credentials = []): bool
     {
-        return $this->userProvider->retrieveByCredentials($credentials) != null;
+        return $this->provider->retrieveByCredentials($credentials) != null;
     }
 
     /**
@@ -75,6 +102,72 @@ class JWTGuard implements Guard
         $this->request = $request;
 
         return $this;
+    }
+
+    /**
+     * Attempt to issue a token for the given credentials.
+     */
+    public function attempt(array $credentials = []): ?Token
+    {
+        $this->fireAttemptEvent($credentials);
+
+        $user = $this->provider->retrieveByCredentials($credentials);
+
+        // If an implementation of UserInterface was returned, we'll ask the provider
+        // to validate the user against the given credentials, and if they are in
+        // fact valid we'll log the users into the application and return true.
+        if ($this->hasValidCredentials($user, $credentials)) {
+            return $this->issue($user);
+        }
+
+        // If the authentication attempt fails we will fire an event so that the user
+        // may be notified of any suspicious attempts to access their account from
+        // an unrecognized user. A developer may listen to this event as needed.
+        $this->fireFailedEvent($user, $credentials);
+
+        return null;
+    }
+
+    /**
+     * Determine if the user matches the credentials.
+     */
+    protected function hasValidCredentials(?Authenticatable $user, array $credentials): bool
+    {
+        return $this->timebox->call(function ($timebox) use ($user, $credentials) {
+            $validated = ! is_null($user) && $this->provider->validateCredentials($user, $credentials);
+
+            if ($validated) {
+                $timebox->returnEarly();
+
+                $this->fireValidatedEvent($user);
+            }
+
+            return $validated;
+        }, 200 * 1000);
+    }
+
+    /**
+     * Fire the attempt event with the arguments.
+     */
+    protected function fireAttemptEvent(array $credentials): void
+    {
+        $this->events->dispatch(new Attempting($this->name, $credentials, false));
+    }
+
+    /**
+     * Fires the validated event if the dispatcher is set.
+     */
+    protected function fireValidatedEvent(Authenticatable $user): void
+    {
+        $this->events->dispatch(new Validated($this->name, $user));
+    }
+
+    /**
+     * Fire the failed authentication attempt event with the given arguments.
+     */
+    protected function fireFailedEvent(?Authenticatable $user, array $credentials): void
+    {
+        $this->events->dispatch(new Failed($this->name, $user, $credentials));
     }
 
     /**
--- tests/JWTGuardTest.php
+++ tests/JWTGuardTest.php
@@ -2,50 +2,175 @@
 
 namespace Tests;
 
+use Illuminate\Auth\Events\Attempting;
+use Illuminate\Auth\Events\Failed;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Events\Dispatcher;
 use Illuminate\Http\Request;
+use Illuminate\Support\Testing\Fakes\EventFake;
 use Leadout\JWT\Blacklists\Drivers\Fake as BlacklistFake;
 use Leadout\JWT\Entities\Token;
 use Leadout\JWT\JWTGuard;
 use Leadout\JWT\TokenManager;
 use Leadout\JWT\TokenProviders\Drivers\Fake as TokenProviderFake;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Tests\Stubs\AuthenticatableStub;
 
 class JWTGuardTest extends TestCase
 {
+    /**
+     * The guard to test.
+     */
+    protected JWTGuard $guard;
+
+    /**
+     * A user provider mock object.
+     */
+    protected MockObject $provider;
+
+    /**
+     * The token manager used for the guard.
+     */
+    protected TokenManager $manager;
+
+    /**
+     * The blacklist used for the manager.
+     */
+    protected BlacklistFake $blacklist;
+
+    /**
+     * The event fake used for the guard.
+     */
+    protected EventFake $events;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->guard = new JWTGuard(
+            'my-guard',
+            $this->provider = $this->createMock(UserProvider::class),
+            $this->manager = new TokenManager(
+                new TokenProviderFake,
+                $this->blacklist = new BlacklistFake,
+                'my-guard',
+                []
+            ),
+            $this->events = new EventFake(new Dispatcher),
+        );
+    }
+
     /** @test */
     public function can_get_the_user()
     {
-        $manager = new TokenManager(new TokenProviderFake, new BlacklistFake, 'my-guard', []);
-
         $user = new AuthenticatableStub('ABC-123');
 
-        $token = $manager->issue($user);
+        $token = $this->manager->issue($user);
 
-        $userProvider = $this->createMock(UserProvider::class);
+        $this->provider->method('retrieveById')->willReturn($user);
 
-        $userProvider->method('retrieveById')->willReturn($user);
+        $this->guard->setRequest($this->getRequest($token));
 
-        $guard = new JWTGuard($this->getRequest($token), $manager, $userProvider);
-
-        $this->assertSame($user, $guard->user());
+        $this->assertSame($user, $this->guard->user());
     }
 
     /** @test */
     public function can_not_get_the_user_if_the_token_is_blacklisted()
     {
-        $manager = new TokenManager(new TokenProviderFake, $blacklist = new BlacklistFake, 'my-guard', []);
-
         $user = new AuthenticatableStub('ABC-123');
 
-        $token = $manager->issue($user);
+        $token = $this->manager->issue($user);
 
-        $blacklist->put($token->claims()->jti(), null);
+        $this->blacklist->put($token->claims()->jti(), null);
 
-        $guard = new JWTGuard($this->getRequest($token), $manager, $this->createMock(UserProvider::class));
+        $this->guard->setRequest($this->getRequest($token));
 
-        $this->assertNull($guard->user());
+        $this->assertNull($this->guard->user());
+    }
+
+    /** @test */
+    public function can_attempt_to_issue_a_token_successfully()
+    {
+        $user = new AuthenticatableStub('ABC-123');
+
+        $this->provider->method('retrieveByCredentials')->willReturn($user);
+
+        $this->provider->method('validateCredentials')->willReturn(true);
+
+        $this->assertEquals(
+            'ABC-123',
+            $this->guard->attempt(['email' => 'john@example.com', 'password' => 'hidden'])->claims()->sub()
+        );
+
+        $this->events->assertDispatched(function (Attempting $event) {
+            return
+                $event->guard == 'my-guard' &&
+                $event->credentials == ['email' => 'john@example.com', 'password' => 'hidden'] &&
+                ! $event->remember;
+        });
+
+        $this->events->assertDispatched(function (Validated $event) use ($user) {
+            return $event->guard == 'my-guard' && $event->user == $user;
+        });
+    }
+
+    /** @test */
+    public function can_attempt_to_issue_a_token_when_the_credentials_cannot_be_validated()
+    {
+        $user = new AuthenticatableStub('ABC-123');
+
+        $this->provider->method('retrieveByCredentials')->willReturn($user);
+
+        $this->provider->method('validateCredentials')->willReturn(false);
+
+        $this->assertNull($this->guard->attempt(['email' => 'john@example.com', 'password' => 'hidden']));
+
+        $this->events->assertDispatched(function (Attempting $event) {
+            return
+                $event->guard == 'my-guard' &&
+                $event->credentials == ['email' => 'john@example.com', 'password' => 'hidden'] &&
+                ! $event->remember;
+        });
+
+        $this->events->assertDispatched(function (Failed $event) use ($user) {
+            return
+                $event->guard == 'my-guard' &&
+                $event->user == $user &&
+                $event->credentials == ['email' => 'john@example.com', 'password' => 'hidden'];
+        });
+    }
+
+    /** @test */
+    public function can_attempt_to_issue_a_token_when_the_credentials_cannot_be_used_for_retrieving()
+    {
+        $this->provider->method('retrieveByCredentials')->willReturn(null);
+
+        $this->assertNull($this->guard->attempt(['email' => 'john@example.com', 'password' => 'hidden']));
+
+        $this->events->assertDispatched(function (Attempting $event) {
+            return
+                $event->guard == 'my-guard' &&
+                $event->credentials == ['email' => 'john@example.com', 'password' => 'hidden'] &&
+                ! $event->remember;
+        });
+
+        $this->events->assertDispatched(function (Failed $event) {
+            return
+                $event->guard == 'my-guard' &&
+                $event->user == null &&
+                $event->credentials == ['email' => 'john@example.com', 'password' => 'hidden'];
+        });
+    }
+
+    /** @test */
+    public function can_get_the_token_manager()
+    {
+        $this->assertSame($this->manager, $this->guard->getTokenManager());
     }
 
     /**
